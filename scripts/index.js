acafa90b037da889e0aad678e6bd2e4b216726ba
LeiderCalvo <leiderstivencalvogomez@hotmail.com>
2020-05-20 13:35:04 -0500

    adding imgs to canvas its done

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,9 @@ const ig_post_sizes = [
     {name: 'storie', w: 1080, h: 1920, ratio: '9:16'},
     {name: 'ig_tv', w: 420*2, h: 654*2, ratio: '1:1.5'}
 ];
+
+const imgs_gallery_urls = ['Crisantemo.jpg', 'Desert.jpg', 'Faro.jpg', 'Fish.jpg',  'Flower.jpg', 'Koala.jpg',  'Penguins.jpg', 'Tulips.jpg'];
+
 var canvas = new fabric.Canvas('canvas');
 
 (e => {
@@ -13,10 +16,21 @@ var canvas = new fabric.Canvas('canvas');
         opt.innerText = ps.name;
         select_post_size.appendChild(opt);
     });
+
     canvas.setWidth( ig_post_sizes[0].w/3 );
     canvas.setHeight( ig_post_sizes[0].h/3 );
     canvas.calcOffset();
+
+    imgs_gallery_urls.forEach( url => addImgToGallery('imgs/'+url) )
+
 })()
+
+function addImgToGallery(url) {
+    let img = document.createElement('img');
+    img.src = url;
+    img.onclick = galleyImgOnClick;
+    imgs_gallery.appendChild(img);
+}
 
 //LISTENERS
 select_post_size.addEventListener('change', e => {
@@ -32,6 +46,28 @@ btn_addtext.onclick = e => {
     canvas.renderAll(); 
 }
 
+function galleyImgOnClick() {
+    fabric.Image.fromURL(this.src, img => {
+        img.scaleToWidth(canvas.width, false);
+        canvas.add(img);
+    });
+}
+
+inpt_fileloader.onchange =  e => {
+    let acceptedFiles = e.target.files;
+    var reader = new FileReader();
+    //let id = Math.random().toString(36).substr(2, 9), acceptedFiles[0].name;
+    
+    reader.onload = e => {
+        /*let p = this.document.querySelector(`#${'p'+id}`);
+        p.innerText = 'Cargado';
+        p.classList.remove('loading');*/
+        addImgToGallery(e.target.result)
+    }
+    reader.readAsDataURL(acceptedFiles[0]);
+    
+}
+
 //////////// Testing 
 
 var rect = new fabric.Rect({
@@ -41,20 +77,15 @@ var rect = new fabric.Rect({
     width: 50,
     height: 50
 });
-
 canvas.add(rect);
 
-fabric.Image.fromURL('../imgs/faro.jpg', img => {
-    img.width = 50;
-    img.height = 50;
-    canvas.add(img);
 
-    img.animate('left', '+=90', {
-        onChange: canvas.renderAll.bind(canvas),
-        duration: 500,
-        easing: fabric.util.ease.easeOutBounce
-    })
-})
+
+/*img.animate('left', '+=90', {
+    onChange: canvas.renderAll.bind(canvas),
+    duration: 500,
+    easing: fabric.util.ease.easeOutBounce
+})*/
 
 btn_right.onclick = e => {
     /*console.log(rect.left)
